e156c96a219a846d0fc5aa48ce2fcea5817a75ae
Gilson <gilpacsebastiao@hotmail.com>
2022-08-01 11:00:01 +0100

    Criei um evento para o jump

--- script.js
+++ script.js
@@ -3,3 +3,9 @@ const cato = document.querySelector(".cato");
 const score = document.querySelector("score");
 let alreadyJump = false;
 let count = 0;
+
+document.addEventListener("keydown", (e) => {
+    if ((e.code === "ArrowUp") | (e.code === "Space")){
+        jump();
+    }
+})

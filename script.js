8e4ca3fa07beeeb353447c0001c32981e23250fc
Gilson <gilpacsebastiao@hotmail.com>
2022-08-01 10:55:21 +0100

    Criei as variáveis personagem, cato, score e um count

---
+++ script.js
@@ -0,0 +1,5 @@
+const personagem = document.querySelector(".personagem");
+const cato = document.querySelector(".cato");
+const score = document.querySelector("score");
+let alreadyJump = false;
+let count = 0;

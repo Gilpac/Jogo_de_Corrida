8103fad7798a5bf879612949a3abfa474608f434
Gilson <gilpacsebastiao@hotmail.com>
2022-08-02 21:42:09 +0100

    Adicionei um setInterval

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const personagem = document.querySelector(".personagem");
 const cato = document.querySelector(".cato");
-const score = document.querySelector("score");
+const score = document.querySelector(".score");
 let alreadyJump = false;
 let count = 0;
 
@@ -27,5 +27,15 @@ function jump() {
         let personagemBottom = parseInt(
             window.getComputedStyle(personagem).getPropertyValue("Bottom")
         );
+        let catoLeft = parseInt(
+            window.getComputedStyle(cato).getPropertyValue("left")
+        );
 
-    })
+        if (catoLeft > 40 && catoLeft < 270 && personagemBottom <= 50 && !alreadyJump){
+            alert(`Fim do Jogo! Sua pontuação foi: ${count}`);
+            count = 0;
+        }
+
+        count ++;
+        score.innerHTML = `Pontuação: ${count}`;
+    }, 10);
